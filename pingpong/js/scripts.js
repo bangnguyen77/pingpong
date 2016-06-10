d357faeb8fd12e324e7ee7d5074b54cc82c692df
Bang Nguyen <bangnv77@gmail.com>
2016-06-10 16:41:38 -0700

    add some final codes to project

--- pingpong/js/scripts.js
+++ pingpong/js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  $("form#numberInput").submit(function(event) {
-    var userNumber = parseInt($("input#userNumber").val());
+  $("#numberInput").submit(function(event) {
+
+    event.preventDefault();
+// user interface logic
+    var userNumber = parseInt($("#userNumber").val());
     var numberRange = [];
 
+// javascript business Logic
     for ( i = 0; i <= userNumber; i++) {
       numberRange.push(i);
-      if (numberRange[i] % 3 === 0 && numberRange[i] % 5 === 0) {
+      if ( numberRange[i] % 3 === 0 && numberRange[i] % 5 === 0 ) {
         numberRange[i] = 'pingpong';
       } else {
         if (numberRange[i] % 5 === 0) {
@@ -14,11 +18,9 @@ $(document).ready(function() {
         if (numberRange[i] % 3 === 0) {
           numberRange[i] = 'ping';
           }
-        numberRange.splice(0, 1, "");
-        $("ul.results").append("<li>" + numberRange[i] + "</li>");
+        numberRange.splice(0, 1, '');
+        $(".results").append("<li>" + numberRange[i] + "</li>");
       }
     }
-    event.preventDefault();
-
   });
 });
