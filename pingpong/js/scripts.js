f2be1901cd1e7ba4413b9066bd4e69118ef43602
Bang Nguyen <bangnv77@gmail.com>
2016-06-10 13:37:14 -0700

    add some initial css rules

--- pingpong/js/scripts.js
+++ pingpong/js/scripts.js
@@ -4,14 +4,18 @@ $(function() {
 
     var userNumber = parseInt($("input#number").val());
 
-    for (i = 1; i <= userNumber; i++) {
-      if (userNumber % 3 === 0) {
-        document.write('ping');
-      } else if (userNumber % 5 === 0){
-        document.write('pong');
-      } else (userNumber % 15 === 0){
+    for (var i = 1; i <= userNumber; i++) {
+      if (i % 3 === 0 && i % 5) {
         document.write('pingpong');
-      }
+      } else {
+        if (i % 3 === 0) {
+          document.write('ping');
+          }
+        if (i% 5 === 0) {
+          document.write('pong');
+          }
+        }
+      document.write('<br>');  
     }
 
 
