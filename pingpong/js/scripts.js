6fbaa29d9845efc767110464f2baf41435ff297b
Bang Nguyen <bangnv77@gmail.com>
2016-06-15 19:33:38 -0700

    update js file

--- pingpong/js/scripts.js
+++ pingpong/js/scripts.js
@@ -1,24 +1,35 @@
+// javascript business logic
+  var pingPong = function (userNumber) {
+
+    var numberRange = [ ];
+
+    for (var i = 1; i <= userNumber; i++) {
+      if ( i % 15 === 0 ) {
+        numberRange.push('pingpong');
+      } else if (i % 5 === 0) {
+          numberRange.push('pong');
+      } else if (i % 3 === 0) {
+          numberRange.push('ping');
+      } else {
+          numberRange.push(i);
+      }
+    };
+    return numberRange;
+};
+
+// user interface logic
 $(document).ready(function() {
   $("form#numberInput").submit(function(event) {
-    var userNumber = parseInt($("input#userNumber").val());
-    var numberRange = [];
 
-    for ( i = 0; i <= userNumber; i++) {
-      numberRange.push(i);
-      if (numberRange[i] % 3 === 0 && numberRange[i] % 5 === 0) {
-        numberRange[i] = 'pingpong';
-      } else {
-        if (numberRange[i] % 5 === 0) {
-          numberRange[i] = 'pong';
-          }
-        if (numberRange[i] % 3 === 0) {
-          numberRange[i] = 'ping';
-          }
-        numberRange.splice(0, 1, "");
-        $("ul.results").append("<li>" + numberRange[i] + "</li>");
-      }
-    }
     event.preventDefault();
+    $("ul#pongResults li").remove();
 
+    var userNumber = parseInt($("input#userNumber").val());
+
+    var numberResults = pingPong(userNumber);
+
+    numberResults.forEach(function(result) {
+      $("#pongResults").append("<li>" + result + "</li>");
+    });
   });
 });
